aa076fed0a0885be5189adb5815c6c7aaf655907
Sandro Esquivel <sae@informatik.uni-kiel.de>
2024-12-07 16:14:05 +0100

    Added day07 solution

---
+++ day07/part_1.py
@@ -0,0 +1,17 @@
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+
+def solve(x, a, y, i):
+    if i == len(a):
+        return x == y
+    if y > x:
+        return False
+    return solve(x, a, y + a[i], i+1) or \
+           solve(x, a, y * a[i], i+1)
+
+res = 0
+for line in lines:
+    x, *a = (int(s) for s in line.replace(":", "").split())
+    if solve(x, a, a[0], 1):
+        res += x
+print(res)
