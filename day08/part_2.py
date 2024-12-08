24ef1057ac79a6bf6211fbf6adddd1a2e5d7813d
Sandro Esquivel <sae@informatik.uni-kiel.de>
2024-12-08 14:14:25 +0100

    Added day08 part 2 solution

---
+++ day08/part_2.py
@@ -0,0 +1,35 @@
+with open('input.txt', 'r') as f:
+    level = [line.strip() for line in f.readlines()]
+
+n, m  = len(level), len(level[0])
+sym = {c for c in "".join(level) if c != "."}
+pos = {c: [(x, y) for x in range(m) for y in range(n) if level[y][x] == c] for c in sym}
+
+def check_pos(x, y):
+    return x >= 0 and x < m and y >= 0 and y < n
+
+antipos = set()
+for c, p in pos.items():
+    for i in range(len(p)-1):
+        for j in range(i+1, len(p)):
+            dx, dy = p[j][0] - p[i][0], p[j][1] - p[i][1]
+            if dx > 0:
+                kmin, kmax = p[i][0] // dx, (m-1-p[i][0]) // dx
+            elif dx < 0:
+                kmin, kmax = (m-1-p[i][0]) // -dx, p[i][0] // -dx
+            if dy > 0:
+                kmin = min(kmin, p[i][1] // dy)
+                kmax = min(kmax, (n-1-p[i][1]) // dy)
+            elif dy < 0:
+                kmin = min(kmin, (n-1-p[i][1]) // -dy)
+                kmax = min(kmax, p[i][1] // -dy)
+            for k in range(-kmin, kmax+1):
+                x, y = p[i][0] + k*dx, p[i][1] + k*dy
+                if check_pos(x, y):
+                    antipos.add((x, y))
+
+#print("\n".join("".join("#" if (x, y) in antipos else level[y][x] \
+#      for x in range(m)) for y in range(n)))
+
+res = len(antipos)
+print(res)
