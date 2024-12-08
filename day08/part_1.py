7b31ec4def851a5865aa0d6929a516d6b823bb65
Sandro Esquivel <sae@informatik.uni-kiel.de>
2024-12-08 14:21:41 +0100

    Updated day08 solution

--- day08/part_1.py
+++ day08/part_1.py
@@ -13,12 +13,10 @@ for c, p in pos.items():
     for i in range(len(p)-1):
         for j in range(i+1, len(p)):
             dx, dy = p[j][0] - p[i][0], p[j][1] - p[i][1]
-            x, y = p[i][0] - dx, p[i][1] - dy
-            if check_pos(x, y):
-                antipos.add((x, y))
-            x, y = p[j][0] + dx, p[j][1] + dy
-            if check_pos(x, y):
-                antipos.add((x, y))
+            for k in (-1, 2):
+                x, y = p[i][0] + k*dx, p[i][1] + k*dy
+                if check_pos(x, y):
+                    antipos.add((x, y))
 
 #print("\n".join("".join("#" if (x, y) in antipos else level[y][x] \
 #      for x in range(m)) for y in range(n)))
