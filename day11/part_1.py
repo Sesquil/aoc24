8b9a895efad1280f93762fb83304075efb5a899b
Sandro Esquivel <sae@informatik.uni-kiel.de>
2024-12-11 08:51:29 +0100

    Added day11 solution

---
+++ day11/part_1.py
@@ -0,0 +1,24 @@
+with open("input.txt", "r") as f:
+    a = [int(x) for x in f.readline().split()]
+
+def blink(a):
+    b = []
+    for x in a:
+        if x == 0:
+            b.append(1)
+        else:
+            s = str(x)
+            l = len(s)
+            if l % 2 == 0:
+                b.append(int(s[:l//2]))
+                b.append(int(s[l//2:]))
+            else:
+                b.append(x * 2024)
+    return b
+
+for _ in range(25):
+    a = blink(a)
+    #print(a)
+
+res = len(a)
+print(res)
---
+++ day11/part_2.py
@@ -0,0 +1,28 @@
+with open("input.txt", "r") as f:
+    a = [int(x) for x in f.readline().split()]
+
+T = {}
+
+def blink(x, n):
+    if (x, n) in T:
+        return T[(x, n)]
+    if n == 0:
+        res = 1
+    elif x == 0:
+        res = blink(1, n-1)
+    else:
+        s = str(x)
+        l = len(s)
+        if l % 2 == 0:
+            res1 = blink(int(s[:l//2]), n-1)
+            res2 = blink(int(s[l//2:]), n-1)
+            res = res1 + res2
+        else:
+            res = blink(x * 2024, n-1)
+    T[(x, n)] = res
+    return res
+
+res = 0
+for x in a:
+    res += blink(x, 75)
+print(res)
